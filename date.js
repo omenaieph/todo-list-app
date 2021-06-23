0869db42cdf5d92492297268ffc7ad1cdb7e5b25
ephraim_omenai <eomenaiofficial@gmail.com>
2021-06-23 12:22:47 +0100

    first commit

---
+++ date.js
@@ -0,0 +1,27 @@
+//jshint esversion:6
+
+exports.getDate = function () {
+    const today = new Date();
+    const options = {
+        weekday: "long",
+        day: "numeric",
+        month: "long",
+    };
+
+
+    return today.toLocaleDateString("en-US", options);
+}
+
+
+
+module.exports.getDay = getDay;
+
+function getDay() {
+    let today = new Date();
+    let options = {
+        weekday: "long"
+    };
+
+
+    return today.toLocaleDateString("en-US", options);
+}
